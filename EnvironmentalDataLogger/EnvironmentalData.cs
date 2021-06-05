f863de0b1000cfb34abf8d1c71769ba4bd32f201
RobinTTY <muellerobin95@gmail.com>
2021-06-05 21:33:57 +0200

    Add EnviromentalDataLogger project

---
+++ EnvironmentalDataLogger/EnvironmentalData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EnvironmentalDataLogger
+{
+    public class EnvironmentalData
+    {
+        /// <summary>
+        /// The time the measurement was taken.
+        /// </summary>
+        public DateTime Timestamp { get; set; }
+        /// <summary>Temperature in degrees Celsius.</summary>
+        public double Temperature { get; set; }
+
+        /// <summary>Relative humidity in percent.</summary>
+        public double Humidity { get; set; }
+
+        /// <summary>Pressure in Pascal.</summary>
+        public double Pressure { get; set; }
+
+        /// <summary>Gas resistance in Ohm.</summary>
+        public double GasResistance { get; set; }
+    }
+}
---
+++ EnvironmentalDataLogger/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Device.I2c;
+using System.Threading.Tasks;
+using Bme680Driver;
+using MongoDB.Driver;
+using Serilog;
+
+namespace EnvironmentalDataLogger
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Database
+            const string connector = "mongodb://192.168.178.143:27017";
+            var mongoClient = new MongoClient(connector);
+            var smartHomeDb = mongoClient.GetDatabase("SmartHome");
+            var environmentalDataCollection = smartHomeDb.GetCollection<EnvironmentalData>("EnvironmentalData");
+
+            // logging
+            using var log = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs/EnvironmentalDataLogger.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            var settings = new I2cConnectionSettings(1, Bme680.SecondaryI2cAddress);
+            var i2CDevice = I2cDevice.Create(settings);
+            using var bme680 = new Bme680(i2CDevice) {GasConversionIsEnabled = false, HeaterIsEnabled = false};
+
+            while (true)
+            {
+                try
+                {
+                    var measurement = await bme680.PerformMeasurementAsync();
+                    var dbData = new EnvironmentalData
+                    {
+                        Timestamp = DateTime.UtcNow,
+                        Temperature = measurement.Temperature,
+                        Pressure = measurement.Pressure,
+                        Humidity = measurement.Humidity,
+                        GasResistance = measurement.GasResistance
+                    };
+                    await environmentalDataCollection.InsertOneAsync(dbData);
+                    await Task.Delay(10000);
+                }
+                catch (Exception exception)
+                {
+                    Log.Error(exception, "Exception occurred during runtime.");
+                }
+            }
+        }
+    }
+}
