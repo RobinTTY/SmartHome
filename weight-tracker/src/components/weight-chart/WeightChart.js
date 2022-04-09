e33d05ac06bbf6eecd6882ebf93f7fdec7515038
RobinTTY <muellerobin95@gmail.com>
2022-04-09 21:05:21 +0200

    Fix graph

--- weight-tracker/src/components/weight-chart/WeightChart.js
+++ weight-tracker/src/components/weight-chart/WeightChart.js
@@ -1,4 +1,5 @@
 import Chart from "chart.js/auto";
+import "chartjs-adapter-date-fns";
 import { useEffect } from "react";
 
 const WeightChart = (props) => {
@@ -11,58 +12,29 @@ const WeightChart = (props) => {
     let date4 = Date.parse("01.04.2020");
     let date5 = Date.parse("01.05.2020");
     let date6 = Date.parse("01.06.2020");
-    let date7 = Date.parse("01.07.2020");
+    let date7 = Date.parse("02.07.2020");
 
     const data = {
       labels: [date1, date2, date3, date4, date5, date6, date7],
       datasets: [
         {
-          label: "My First dataset",
-          backgroundColor: "#8e5ea2",
-          borderColor: "#3e95cd",
+          label: "Weight",
+          backgroundColor: "#5580FF",
+          borderColor: "#80AAFF",
           fill: false,
           data: [10, 20, 30, 40, 50, 60, 70],
         },
-        {
-          label: "My Second dataset",
-          backgroundColor: "#3e95cd",
-          borderColor: "#8e5ea2",
-          fill: false,
-          data: [70, 60, 50, 40, 30, 20, 10],
-        },
-        {
-          label: "Dataset with point data",
-          backgroundColor: "#3cba9f",
-          borderColor: "#3cba9f",
-          fill: false,
-          data: [
-            {
-              x: date1,
-              y: 10,
-            },
-            {
-              x: date2,
-              y: 20,
-            },
-            {
-              x: date3,
-              y: 30,
-            },
-            {
-              x: date4,
-              y: 40,
-            },
-          ],
-        },
       ],
     };
+
+    // config
     const config = {
       type: "line",
       data: data,
       options: {
         plugins: {
           title: {
-            text: "Chart.js Time Scale",
+            text: "Weight over time",
             display: true,
           },
         },
@@ -70,6 +42,7 @@ const WeightChart = (props) => {
           x: {
             type: "time",
             time: {
+              display: true,
               unit: "day",
             },
           },
@@ -83,8 +56,9 @@ const WeightChart = (props) => {
       },
     };
 
-    const myChart = new Chart(document.getElementById(chartId), config);
-  });
+    // construct chart
+    new Chart(document.getElementById(chartId), config);
+  }, []);
 
   return (
     <>
